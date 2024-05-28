668500509b43e8b74292c55203ccd5747cc499a0
Dalois-30 <claudetapi@yahoo.com>
2024-05-28 16:32:57 +0100

    change expiration of the token

--- src/auth/constant/constants.ts
+++ src/auth/constant/constants.ts
@@ -1,7 +1,8 @@
 
 import * as crypto from 'crypto';
 
-export const EXPIRES_IN = 3600;
+export const EXPIRES_IN = 2592000; // one month
+// export const EXPIRES_IN = 3600;
 export const generateRandomString = (length: number) => {
     return crypto.randomBytes(length).toString('hex').slice(0, length);
   }

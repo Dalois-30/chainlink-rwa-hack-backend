4a1ad877519d1e3f2eda7036bb3a5fcfdb025c13
Dalois-30 <claudetapi@yahoo.com>
2024-05-29 23:34:18 +0100

    update docker file and github action file and set the real value of default aws region

--- configure-aws-cli.sh
+++ configure-aws-cli.sh
@@ -6,7 +6,7 @@ source /opt/aws-cli-env/bin/activate
 # Configure AWS CLI with the provided environment variables
 aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
 aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
-aws configure set region "$AWS_DEFAULT_REGION"
+aws configure set region "$AWS_REGION"
 
 # Verify AWS CLI configuration
 aws configure list
